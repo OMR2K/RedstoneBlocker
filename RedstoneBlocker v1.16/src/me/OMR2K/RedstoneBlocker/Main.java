88857404989509db1ce621af5969efeb8684326e
OMR2K <qtr.omr@gmail.com>
2021-01-16 16:01:07 +0300

    Added ability to toggle which blocks should be blocked in the config.yml

--- RedstoneBlocker v1.16/src/me/OMR2K/RedstoneBlocker/Main.java
+++ RedstoneBlocker v1.16/src/me/OMR2K/RedstoneBlocker/Main.java
@@ -45,22 +45,199 @@ public class Main extends JavaPlugin implements Listener
 	{
 		
 		TimelessPlayer player = new TimelessPlayer(e.getPlayer());
+
+		// Getting blocked items from the config, and assigning the boolean values
+		boolean blockRedstoneDust = getConfig().getBoolean("redstone_dust");
+		boolean blockRedstoneRepeater = getConfig().getBoolean("redstone_repeater");
+		boolean blockRedstoneComparator = getConfig().getBoolean("redstone_comparator");
+		boolean blockRedstoneTorch = getConfig().getBoolean("redstone_torch");
+		boolean blockRedstoneBlock = getConfig().getBoolean("redstone_block");
+		boolean blockPiston = getConfig().getBoolean("piston");
+		boolean blockStickyPiston = getConfig().getBoolean("sticky_piston");
+		boolean blockDispenser = getConfig().getBoolean("dispenser");
+		boolean blockDropper = getConfig().getBoolean("dropper");
+		boolean blockObserver = getConfig().getBoolean("observer"); 
+		boolean blockLever = getConfig().getBoolean("lever");
+		boolean blockPressurePlates = getConfig().getBoolean("pressure_plates");
+		boolean blockTripwireHook = getConfig().getBoolean("tripwire_hook");
+		boolean blockButtons = getConfig().getBoolean("buttons");
+		boolean blockDaylightDetector = getConfig().getBoolean("daylight_detector");
+		boolean blockHopper = getConfig().getBoolean("hopper");
+		boolean blockTarget = getConfig().getBoolean("target");
+		boolean blockTNT = getConfig().getBoolean("tnt");
 		
-		if ( (e.getBlock().getType() == Material.REDSTONE_WIRE 
-				|| e.getBlock().getType() == Material.REDSTONE_BLOCK 
-				|| e.getBlock().getType() == Material.REDSTONE_WALL_TORCH 
-				|| e.getBlock().getType() == Material.PISTON 
-				|| e.getBlock().getType() == Material.STICKY_PISTON 
-				|| e.getBlock().getType() == Material.COMPARATOR 
-				|| e.getBlock().getType() == Material.DISPENSER 
-				|| e.getBlock().getType() == Material.LEVER )
-				&& ( (player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")))
-		{
-			e.setCancelled(true);
-			
-			e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place redstone.");
-		}
+		// Canceling the placing event for blocked blocks in the config file
+		if (blockRedstoneDust 
+				&& (e.getBlock().getType() == Material.REDSTONE_WIRE)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
 		
+		else if (blockRedstoneRepeater 
+				&& (e.getBlock().getType() == Material.REPEATER)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockRedstoneComparator
+				&& (e.getBlock().getType() == Material.COMPARATOR)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockRedstoneTorch 
+				&& (e.getBlock().getType() == Material.REDSTONE_WALL_TORCH)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockRedstoneBlock 
+				&& (e.getBlock().getType() == Material.REDSTONE_BLOCK)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockPiston
+				&& (e.getBlock().getType() == Material.PISTON)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockStickyPiston
+				&& (e.getBlock().getType() == Material.STICKY_PISTON)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockDispenser
+				&& (e.getBlock().getType() == Material.DISPENSER)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockDropper
+				&& (e.getBlock().getType() == Material.DROPPER)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockObserver
+				&& (e.getBlock().getType() == Material.OBSERVER)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockLever
+				&& (e.getBlock().getType() == Material.LEVER)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockRedstoneBlock 
+				&& (e.getBlock().getType() == Material.REDSTONE_BLOCK)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockPressurePlates
+				&& (e.getBlock().getType() == Material.OAK_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.STONE_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.SPRUCE_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.BIRCH_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.JUNGLE_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.ACACIA_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.DARK_OAK_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.CRIMSON_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.WARPED_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.POLISHED_BLACKSTONE_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.LIGHT_WEIGHTED_PRESSURE_PLATE
+					|| e.getBlock().getType() == Material.HEAVY_WEIGHTED_PRESSURE_PLATE)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockTripwireHook
+				&& (e.getBlock().getType() == Material.TRIPWIRE_HOOK)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockButtons
+				&& (e.getBlock().getType() == Material.OAK_BUTTON
+					|| e.getBlock().getType() == Material.STONE_BUTTON
+					|| e.getBlock().getType() == Material.SPRUCE_BUTTON
+					|| e.getBlock().getType() == Material.BIRCH_BUTTON
+					|| e.getBlock().getType() == Material.JUNGLE_BUTTON
+					|| e.getBlock().getType() == Material.ACACIA_BUTTON
+					|| e.getBlock().getType() == Material.DARK_OAK_BUTTON
+					|| e.getBlock().getType() == Material.CRIMSON_BUTTON
+					|| e.getBlock().getType() == Material.WARPED_BUTTON
+					|| e.getBlock().getType() == Material.POLISHED_BLACKSTONE_BUTTON)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockDaylightDetector
+				&& (e.getBlock().getType() == Material.DAYLIGHT_DETECTOR)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockHopper
+				&& (e.getBlock().getType() == Material.HOPPER)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockTarget
+				&& (e.getBlock().getType() == Material.TARGET)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
+		
+		else if (blockTNT
+				&& (e.getBlock().getType() == Material.TNT)
+				&& ((player.getDays() * 24 + player.getHours()) < getConfig().getInt("hours")) )
+			{
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "You need at least " + getConfig().getInt("hours") + " hour(s) of playtime to place this.");
+			}
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
